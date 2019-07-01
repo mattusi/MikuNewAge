667adf85064c2cc6b7a7d9b9a6656c57e8a1a0aa
mattusi <matheustusi@gmail.com>
2019-06-30 22:50:01 -0300

    First Commit

---
+++ MikuNewAge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MikuNewAge
+//
+//  Created by Matheus Santos on 30/06/19.
+//  Copyright © 2019 Matheus Santos. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
